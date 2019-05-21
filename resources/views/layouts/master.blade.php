c65ec38be0c7c3b0a7e20cc897e5b696b4a9e028
aoyama <aoyama@mediowl.co.jp>
2019-05-21 11:26:52 +0900

    modified css path

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>@yield('title')</title>
-    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="/css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <h1>@yield('header')</h1>
