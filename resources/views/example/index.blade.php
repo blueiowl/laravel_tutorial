d820d242c1da593e28c815ee959ead074762b72d
aoyama <aoyama@mediowl.co.jp>
2019-05-21 11:26:14 +0900

    modified read function to display todolist only for logined users

--- resources/views/example/index.blade.php
+++ resources/views/example/index.blade.php
@@ -6,43 +6,38 @@
     作業一覧
 @endsection
 
-
-@section('subheader')
 @if(Auth::check())
-    <p><a href="/create/{{ $user->id }}">新規作業作成</a></p>
+    @section('subheader')
+        <p><a href="/create/{{ $user->id }}">新規作業作成</a></p>
+
+    <p>
+        <form method="GET" action="/search">
+            <label>作業内容検索</label>
+            <input type="text" name="content">
+            <input type="submit" value="検索">
+        </form>
+    </p>
+    @endsection
 @endif
-<p>
-    <form method="GET" action="/search">
-        <label>作業内容検索</label>
-        <input type="text" name="content">
-        <input type="submit" value="検索">
-    </form>
-</p>
-@endsection
 
 @section('content')
-
     @if(Auth::check())
         <p>
             USER: {{ $user->name . '(' . $user->email .')' }}|
             <a href="/home">home(ここからログアウトできます)</a>
         </p>
+        @foreach ($examples as $example)
+            <p>
+                <a href="/show/{{ $example->id }}">{{ $example->workName }}</a>
+                [{{ $example->status }}][{{ $example->updated_at }}] 
+                <a href="/edit/{{ $example->id }}/{{ $user->id }}"> [編集]</a>
+                <a href="/delete/{{ $example->id }}"> [削除]</a>
+            </p>
+        @endforeach
     @else
         <p>
             *ログインしていません。(<a href="/login">ログイン</a>)|
             <a href="/register">登録</a>
         </p>
     @endif
-
-    @foreach ($examples as $example)
-        <p>
-            <a href="/show/{{ $example->id }}">{{ $example->workName }}</a>
-            [{{ $example->status }}][{{ $example->updated_at }}] 
-            @if(Auth::check())
-                <a href="/edit/{{ $example->id }}/{{ $user->id }}"> [編集]</a>
-                <a href="/delete/{{ $example->id }}"> [削除]</a>
-            @endif
-        </p>
-    @endforeach
-
 @endsection
